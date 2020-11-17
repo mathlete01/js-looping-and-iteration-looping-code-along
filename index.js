135a1b4d7b423a6a86e4786d463427acdcee622b
mathlete01 <msallin@gmail.com>
2020-11-16 16:47:02 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+// function writeCards(namesArr, event) {
+//     for (let i = 0; i < namesArr.length; i ++) {
+//         console.log (`Thank you, ${namesArr[i]}, for the wonderful ${event} gift!`);
+//     }
+// }
+
+function writeCards(namesArr, event) {
+    let i = 0;
+    while (i < namesArr.length) {
+        console.log(`Thank you, ${namesArr[i]}, for the wonderful ${event} gift!`);
+        i++;
+    }
+}
+
+// writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+function countDown(num) {
+    let i = 0;
+    while (i < (num + 1)) {
+        console.log(num - i);
+        i ++;
+    }
+}
+
+// countDown(20)
